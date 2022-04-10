5f13c962faac887f7c80306e000e6d7ba4502b9b
Tim Terry <t_terry79@hotmail.com>
2022-04-10 17:39:27 +0100

    Java Gradle sample project

---
+++ samples/java-gradle/run.sh
@@ -0,0 +1,1 @@
+docker run -it -v "$PWD":/app -w /app gradle:7.4.2-jdk17 gradle test
---
+++ samples/java-gradle/src/main/java/org/tterry/javaGradle/Thing.java
@@ -0,0 +1,8 @@
+package org.tterry.javaGradle;
+
+public class Thing {
+
+    public boolean doThing(){
+        return true;
+    }
+}
