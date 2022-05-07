51eb256c2b9ac283d06f578d30724f7a353b159d
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-07 23:46:32 +0100

    reduce code

--- app/main.py
+++ app/main.py
@@ -14,12 +14,7 @@ app.register_blueprint(github_bp, url_prefix="/oauth")
 @app.cli.command()
 def routes():
     'Display registered routes'
-    rules = []
     for rule in app.url_map.iter_rules():
         methods = ','.join(sorted(rule.methods))
-        rules.append((rule.endpoint, methods, str(rule)))
-
-    sort_by_rule = operator.itemgetter(2)
-    for endpoint, methods, rule in sorted(rules, key=sort_by_rule):
-        route = '{:50s} {:25s} {}'.format(endpoint, methods, rule)
+        route = '{:50s} {:25s} {}'.format(rule.endpoint, methods, str(rule))
         print(route)
