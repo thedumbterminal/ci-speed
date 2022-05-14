dc1caf1e3b96d740d4a2a183e814936eaa920520
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-14 18:11:59 +0100

    support creds on xhr requests

--- app/f_app.py
+++ app/f_app.py
@@ -4,7 +4,11 @@ import os
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True, origins=[
+    'http://localhost:5000',
+    'http://localhost:3000',
+    'https://thedumbterminal.github.io/ci-speed-ui/'
+])
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "reallysecret")
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
