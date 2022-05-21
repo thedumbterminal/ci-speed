d974980dba9ed37d5108347459ec2c5bda3d2c7f
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-21 01:52:55 +0100

    ui proxy now settng the correct content type

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from models import security, user_datastore
 from db import db
 from requests import get
 import os
+from flask import make_response
 
 ma = Marshmallow(app)
 
@@ -19,6 +20,14 @@ security.init_app(app, user_datastore)
 ui_url_base = os.environ.get("UI_URL_BASE", "http://localhost:3000/")
 ui_url_path = os.environ.get("UI_URL_PATH", "")
 
+def _proxy_request(url):
+    print('Proxy to:', url)
+    proxy_response = get(url)
+    response = make_response(proxy_response.content)
+    response.headers['Content-Type'] = proxy_response.headers['Content-Type']
+    return response
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
@@ -26,30 +35,25 @@ def proxy(path):
     if path == '':
         path = ui_url_path
     new = f'{ui_url_base}{path}'
-    print('Proxy to:', new)
-    return get(new).content
+    return _proxy_request(new)
 
 
 @app.route('/static/js/<path:path>')
 def static_js_proxy(path):
     new = f'{ui_url_base}{ui_url_path}static/js/{path}'
-    print('Proxy to:', new)
-    return get(new).content
+    return _proxy_request(new)
 
 
 @app.route('/static/media/<path:path>')
 def static_media_proxy(path):
     new = f'{ui_url_base}{ui_url_path}static/media/{path}'
-    print('Proxy to:', new)
-    return get(new).content
+    return _proxy_request(new)
 
 
 @app.route('/static/css/<path:path>')
 def static_css_proxy(path):
     new = f'{ui_url_base}{ui_url_path}static/css/{path}'
-    print('Proxy to:', new)
-    return get(new).content
-
+    return _proxy_request(new)
 
 @app.cli.command()
 def routes():
