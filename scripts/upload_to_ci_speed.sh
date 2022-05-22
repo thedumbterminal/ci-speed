bf6b9d3ca7cf054d98e57ff5ee87557f2fb4cea6
thedumbterminal <github@thedumbterminal.co.uk>
2022-05-22 21:13:52 +0100

    added curl to api

--- scripts/upload_to_ci_speed.sh
+++ scripts/upload_to_ci_speed.sh
@@ -9,3 +9,12 @@ PROJECT_NAME=$1
 BUILD_REF=$2
 
 echo "Project: ${PROJECT_NAME} Build: ${BUILD_REF}"
+
+curl -X 'POST' \
+  'https://ci-speed.herokuapp.com/api/test_runs/' \
+  -H 'accept: application/json' \
+  -H 'Authentication-Token: ${CI_SPEED_AUTH_TOKEN}' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'file=@test_results.xml;type=text/xml' \
+  -F 'project_name=${PROJECT_NAME}' \
+  -F 'build_ref=${BUILD_REF}'
