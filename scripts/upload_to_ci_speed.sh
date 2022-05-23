eb8d15780b3a8eed55abe27e09c86e3f16050914
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-23 19:06:05 +0100

    corrected upload script

--- scripts/upload_to_ci_speed.sh
+++ scripts/upload_to_ci_speed.sh
@@ -8,13 +8,16 @@ PROJECT_NAME=$1
 
 BUILD_REF=$2
 
+HOST=https://ci-speed.herokuapp.com
+#HOST=http://localhost:5000
+
 echo "Project: ${PROJECT_NAME} Build: ${BUILD_REF}"
 
-curl --fail-with-body -X 'POST' \
-  'https://ci-speed.herokuapp.com/api/test_runs/' \
-  -H 'accept: application/json' \
-  -H 'Authentication-Token: ${CI_SPEED_AUTH_TOKEN}' \
-  -H 'Content-Type: multipart/form-data' \
-  -F 'file=@test_results.xml;type=text/xml' \
-  -F 'project_name=${PROJECT_NAME}' \
-  -F 'build_ref=${BUILD_REF}'
+curl --fail-with-body -X "POST" \
+  "${HOST}/api/test_runs/" \
+  -H "accept: application/json" \
+  -H "Authentication-Token: ${CI_SPEED_AUTH_TOKEN}" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@test_results.xml;type=text/xml" \
+  -F "project_name=${PROJECT_NAME}" \
+  -F "build_ref=${BUILD_REF}"
