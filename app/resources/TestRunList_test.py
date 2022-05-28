2821c1417cd386c078e075c0d59937807e3e9ddf
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-28 17:12:44 +0100

    added another test

---
+++ app/resources/TestRunList_test.py
@@ -0,0 +1,10 @@
+from .TestRunList import TestRunList as ClassUnderTest
+import pytest
+
+@pytest.mark.skip(reason="Not finished")
+def test_junit_to_test_run():
+    build_id = 1
+    junit = {}
+    testRunList = ClassUnderTest()
+    test_run = testRunList._junit_to_test_run(build_id, junit)
+    assert test_run == True
