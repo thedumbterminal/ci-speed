6c04031ce69e43727aab3caf47afda7d9682afc1
thedumbterminal <github@thedumbterminal.co.uk>
2022-06-15 21:01:18 +0100

    now supporting test time stats

--- app/resources/Project.py
+++ app/resources/Project.py
@@ -2,7 +2,7 @@ from flask_restx import Resource, Namespace
 from models import Project as ProjectModel, Build
 from schemas import ProjectSchema
 from flask_security import auth_required
-from schemas import BuildSchema
+from schemas import BuildSchema, TestSuiteSchema
 
 
 api = Namespace("projects", description="Project related operations")
@@ -45,3 +45,32 @@ class ProjectNumTests(Resource):
     def get(self, project_id):
         """Retrieve a project's test number history"""
         return self._get_num_tests(project_id)
+
+
+@api.route("/<int:project_id>/test_duration")
+@api.param("project_id", "The project identifier")
+class ProjectNumTests(Resource):
+    def _get_test_duration(self, project_id):
+        results = []
+
+        build_schema = BuildSchema()
+        test_suite_schema = TestSuiteSchema()
+        builds = Build.query.filter_by(project_id=project_id).all()
+        for build in builds:
+            print(build)
+            serialised_build = build_schema.dump(build)
+            result = {"x": serialised_build["created_at"], "y": 0}
+            for test_run in build.test_runs:
+                for test_suite in test_run.test_suites:
+                    serialised_test_suite = test_suite_schema.dump(test_suite)
+                    result["y"] += serialised_test_suite["time"]
+            results.append(result)
+        print(results)
+        return results
+
+    @api.doc("get_project test duration history")
+    @auth_required("token", "session")
+    @api.doc(security=["apikey"])
+    def get(self, project_id):
+        """Retrieve a project's test duration history"""
+        return self._get_test_duration(project_id)
