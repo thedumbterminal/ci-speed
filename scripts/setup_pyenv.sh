1a0cbab82fe32bccf4af27a36903d557e89bae07
The Dumb Terminal <github@thedumbterminal.co.uk>
2023-01-28 13:09:38 +0000

    removed pinning

--- scripts/setup_pyenv.sh
+++ scripts/setup_pyenv.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 PYTHONVERSION=`cat .python-version | grep -E -o "^[^/]+"`
-VIRTUALENV=`cat .python-version`
-pyenv virtualenv ${PYTHONVERSION} ${VIRTUALENV}
+VIRTUALENVSHORT=`cat .python-version | grep -E -o "[^/]+$"`
+VIRTUALENVFULL=`cat .python-version`
+PYENV_VERSION=${PYTHONVERSION}
+pyenv virtualenv-delete -f ${VIRTUALENVSHORT}
+pyenv virtualenv ${PYTHONVERSION} ${VIRTUALENVFULL}
+unset PYENV_VERSION
 pyenv version
