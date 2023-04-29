743a3475dbfcb0b58cd99b75d9755bcc96c0cad5
The Dumb Terminal <github@thedumbterminal.co.uk>
2023-04-29 15:33:22 +0100

    working on org repos

--- app/lib/repo.py
+++ app/lib/repo.py
@@ -1,11 +1,23 @@
 from flask_dance.contrib.github import github
-
+from yarl import URL
 
 def repo_list():
-    repos_resp = github.get("/user/repos")
+    url = URL.build(
+        scheme="https",
+        host="api.github.com",
+        path="/user/repos",
+        query={
+            "per_page": 100
+        }
+    )
+    print('url', url)
+    relativeUrl = url.path_qs
+
+    repos_resp = github.get(relativeUrl)
     if not repos_resp.ok:
         print("Failed to fetch user repos.")
         return []
     repos = repos_resp.json()
+    print('num repos found: ', len(repos))
     repo_names = list(map(lambda x: x["full_name"], repos))
     return repo_names
