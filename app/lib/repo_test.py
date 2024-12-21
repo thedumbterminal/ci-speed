07dce1bb23aa8ceba1edd64ade9904733e79d737
The Dumb Terminal <github@thedumbterminal.co.uk>
2024-12-21 12:57:44 +0000

    lint

--- app/lib/repo_test.py
+++ app/lib/repo_test.py
@@ -1,16 +1,15 @@
 from lib.repo import repo_list
 import pytest
 
+
 @pytest.fixture
 def mock_function(mocker):
     return mocker.patch(
         "lib.repo._wrapped_get",
-        return_value=[
-            {"full_name": "test1"}
-        ],
+        return_value=[{"full_name": "test1"}],
     )
 
 
 def test_repo_list(mock_function):
     names = repo_list()
-    assert names == ['test1']
+    assert names == ["test1"]
