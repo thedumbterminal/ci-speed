cfa7406a90c32dc1cac6f3b09e4bd1105093a5a4
The Dumb Terminal <github@thedumbterminal.co.uk>
2024-12-21 12:56:54 +0000

    added more test coverage

---
+++ app/lib/repo_test.py
@@ -0,0 +1,16 @@
+from lib.repo import repo_list
+import pytest
+
+@pytest.fixture
+def mock_function(mocker):
+    return mocker.patch(
+        "lib.repo._wrapped_get",
+        return_value=[
+            {"full_name": "test1"}
+        ],
+    )
+
+
+def test_repo_list(mock_function):
+    names = repo_list()
+    assert names == ['test1']
